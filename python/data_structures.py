aa28693d1f4f5f46ba0624a33a61230a1b7c1a7f
kingcabrams <abramscma@gmail.com>
2019-08-05 17:21:28 -0400

    Adding references for core python

--- python/data_structures.py
+++ python/data_structures.py
@@ -4,7 +4,6 @@ nums = [1, 5, 2, 4, 3]
 
 
 def main():
-
     """Lists"""
     courses.append('Art')
     print(courses)
@@ -32,10 +31,58 @@ def main():
     print(sum(nums))
 
     print(courses.index('CompSci'))  # Prints index of first occurrence
+    for index, course in enumerate(courses, start=1):
+        print(index, course)
+
+    course_str = ', '.join(courses)  # List to string
+
+    new_list = course_str.split(', ')
+
+    print(course_str)
+    print(new_list)
 
     print(courses[0:2])
     print(courses[:2])  # Assumes first value as first index
     print(courses[1:])  # Assumes second value as last index
+
+    # Downside of mutable objects
+    new_list2 = new_list
+
+    new_list2[0] = 'English'  # Changes both lists
+
+    print(new_list2)
+    print(new_list)
+
+    # Empty Lists
+    empty_list = []
+    empty_list = list()
+
+    """Tuples"""
+    tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+    tuple_2 = tuple_1
+
+    print(tuple_1)
+    print(tuple_2)
+
+    # tuple_1[0] = 'Art'  # Can't modify tuple it is immutable
+
+    # Empty Tuples
+    empty_tuple = ()
+    empty_tuple = tuple()
+
+    """Sets"""
+    cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
+    art_courses = {'History', 'Math', 'Art', 'Design'}
+
+    print(cs_courses)  # Order doesn't matter in sets
+
+    print(cs_courses.intersection(art_courses))
+    print(cs_courses.difference(art_courses))
+    print(cs_courses.union(art_courses))
+
+    # Empty Sets
+    empty_set = {}  # This isn't right! This is an empty dictionary
+    empty_set = set()
 
 
 main()
